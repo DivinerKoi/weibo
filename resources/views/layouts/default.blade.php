6408287b8c6eb75ecd566702dfd30b7a75854225
DivinerKoi <you@example.com>
2021-10-20 16:24:55 +0800

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,14 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>@yield('title','weibo APP')- Laravel 新手入门教程</title>
-	</head>
-	<body>
-        @yield('content')
-	</body>
-</html> -->
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -16,16 +5,13 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container"> <a class="navbar-brand" href="/">Weibo App</a>
-                <ul class="navbar-nav justify-content-end">
-                    <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-                </ul>
-            </div>
-        </nav>
+        @include('layouts._header')
+        
         <div class="container">
-        @yield('content')
+            <div class="offset-md-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+            </div>
         </div>
     </body>
 </html>
