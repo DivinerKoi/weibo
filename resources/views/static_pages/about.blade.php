25c4b300c5ecba2f230d279ff9f155df2ddc8a6c
DivinerKoi <you@example.com>
2021-10-20 12:58:06 +0800

    初始化样式

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,16 @@
+<!-- <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+        <h1>这是关于页</h1>
+	</body>
+</html> -->
+
+@extends('layouts.default')
+@section('title','关于')
+@section('content')
+<h1 style="color: #0062CC;">这也是关于页</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,16 @@
+<!-- <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+        <h1>这是帮助页</h1>
+	</body>
+</html> -->
+
+@extends('layouts.default')
+@section('title','帮助')
+@section('content')
+<h1>这也是帮助页</h1>
+@stop
