a12d1f2a5bacc87092e3dd46acdd878381e75065
DivinerKoi <you@example.com>
2021-10-23 15:52:35 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,13 +14,22 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show',compact('user'));//通过 compact 方法转化为一个关联数组
     }
-    
-    public function store(Request $request) { 
-        $this->validate($request, [ 
-            'name' => 'required|unique:users|max:50', 
-            'email' => 'required|email|unique:users|max:255', 
-            'password' => 'required|confirmed|min:6' 
-        ]); 
-        return; 
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        
+        return redirect()->route('users.show', [$user]);
     }
 }
