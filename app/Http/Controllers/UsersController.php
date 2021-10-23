95a88c3612d2f3fdbbf9fad0e7a853d42bf119f1
DivinerKoi <you@example.com>
2021-10-23 14:56:47 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,4 +14,13 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show',compact('user'));//通过 compact 方法转化为一个关联数组
     }
+    
+    public function store(Request $request) { 
+        $this->validate($request, [ 
+            'name' => 'required|unique:users|max:50', 
+            'email' => 'required|email|unique:users|max:255', 
+            'password' => 'required|confirmed|min:6' 
+        ]); 
+        return; 
+    }
 }
